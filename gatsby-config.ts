b839fdb80fc5d7ea3a93aa475cdcab0ad2e810fb
kyounghee kim <nokdoo00@gmail.com>
2024-04-10 16:13:53 -0400

    set for TS and, update plugin eslint and perttier

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,14 +1,34 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `My Gatsby Blog`,
+    siteUrl: ``,
+    description: ``,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-alias-imports",
+      options: {
+        alias: {
+          "@src": "src",
+          "@components": "src/components",
+          "@layouts": "src/layouts",
+          "@api": "src/api",
+          "@pages": "src/pages",
+          "@styles": "src/styles",
+          "@utils": "src/utils",
+          "@hooks": "src/hooks",
+          "@images": "src/images",
+          "@types": "src/types",
+          "@templates": "src/templates",
+          "@posts": "content/posts",
+        },
+      },
+    },
+  ],
+};
 
-export default config
+export default config;
