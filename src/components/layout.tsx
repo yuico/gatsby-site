c05f517855f627836e75c3abbebe7e0944a0af0d
kyounghee kim <nokdoo00@gmail.com>
2024-04-10 15:04:02 -0400

    style compoment via CSS modules on Layout comp

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,12 @@
 import React, { ReactNode } from 'react'
 import { Link } from 'gatsby'
+import { 
+    container,
+    heading,
+    navLinks,
+    navLinkItem,
+    navLinkText 
+} from './layout.module.css'
 
 interface LayoutProps{
     pageTitle: string
@@ -8,15 +15,19 @@ interface LayoutProps{
 
 const Layout: React.FC<LayoutProps> = ({ pageTitle, children }) => {
     return (
-        <div>
+        <div className={container}>
             <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}>
+                        <Link className={navLinkText} to="/">Home</Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link className={navLinkText} to="/about">About</Link>
+                    </li>
                 </ul>
             </nav>
             <main>
-                <h1>{pageTitle}</h1>
+                <h1 className={heading}>{pageTitle}</h1>
                 {children}
             </main>
         </div>
