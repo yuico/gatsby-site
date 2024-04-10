77841a82c3ab9505456973d47b9f800ecea86b18
kyounghee kim <nokdoo00@gmail.com>
2024-04-10 16:19:47 -0400

    Revert "set for TS and, update plugin eslint and perttier"
    This reverts commit b839fdb80fc5d7ea3a93aa475cdcab0ad2e810fb.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,34 +1,14 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `My Gatsby Blog`,
-    siteUrl: ``,
-    description: ``,
+    siteUrl: `https://www.yourdomain.tld`,
   },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [
-    "gatsby-plugin-sass",
-    {
-      resolve: "gatsby-plugin-alias-imports",
-      options: {
-        alias: {
-          "@src": "src",
-          "@components": "src/components",
-          "@layouts": "src/layouts",
-          "@api": "src/api",
-          "@pages": "src/pages",
-          "@styles": "src/styles",
-          "@utils": "src/utils",
-          "@hooks": "src/hooks",
-          "@images": "src/images",
-          "@types": "src/types",
-          "@templates": "src/templates",
-          "@posts": "content/posts",
-        },
-      },
-    },
-  ],
-};
+  plugins: [],
+}
 
-export default config;
+export default config
