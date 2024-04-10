9b944245aa4d0e3d055a79912e4f93841a64ddde
kyounghee kim <nokdoo00@gmail.com>
2024-04-10 16:57:11 -0400

    add plugin to use free image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>Home page description</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        //"../images/clifford.jpg"
+      />
     </Layout>
   )
 }
